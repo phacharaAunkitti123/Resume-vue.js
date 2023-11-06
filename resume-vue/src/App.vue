854d0bf0ebb23803733029e151526edfb7c6c1ab
phacharaAunkitti123 <phacharaaunkitti816@gmail.com>
2023-11-07 02:30:34 +0700

    create resume

---
+++ resume-vue/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <h1>{{ yourName }}</h1>
+    <p>{{ yourAddress }}</p>
+    <p>{{ yourCity }}, {{ yourState }} {{ yourZIPCode }}</p>
+    <p>{{ yourPhoneNumber }}</p>
+    <p>{{ yourEmailAddress }}</p>
+    <a :href="yourLinkedInURL">LinkedIn Profile</a>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      yourName: "Your Name",
+      yourAddress: "Your Address",
+      yourCity: "Your City",
+      yourState: "Your State",
+      yourZIPCode: "Your ZIP Code",
+      yourPhoneNumber: "Your Phone Number",
+      yourEmailAddress: "Your Email Address",
+      yourLinkedInURL: "Your LinkedIn Profile URL",
+    };
+  },
+};
+</script>
+
+<style scoped>
+h1 {
+  font-size: 24px;
+  color: #007BFF;
+}
+
+p {
+  font-size: 16px;
+}
+
+a {
+  text-decoration: none;
+  color: #007BFF;
+}
+</style>
